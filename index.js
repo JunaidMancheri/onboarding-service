e5ac0c3a4dd3c448378e0eb473c30949852b8f14
Junaid <junaidofficialnow@gmail.com>
2024-12-01 17:47:05 +0530

    feat: implemented speech  to text functionality

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ const { Server } = require('socket.io');
 const http = require('http');
 const { LLMChat } = require('./llm-config/chat');
 const { getTTSAudioContent } = require('./text-to-speech');
-require('./llm-config')
+const {
+  transcribeAudio,
+} = require('./speech-to-text');
+require('./llm-config');
 
 const httpServer = http.createServer();
 const socketServer = new Server(httpServer, { cors: { origin: '*' } });
 
 const onboardingSocket = socketServer.of('/onboarding');
-
 onboardingSocket.on('connection', async socket => {
-  console.log('Onboarding user started');
   const llmChat = new LLMChat();
   socket.emit('welcome', await llmChat.sendGreetings());
 
-  socket.on('audio', async (data) => {
-    console.log(data);
-  })
 
-  socket.on('message', async msg => {
-    const llmResponse = await  llmChat.interactWithLLM(msg);
+  socket.on('audio', async audioChunk => {
+    const transcribedText = await transcribeAudio(audioChunk);
+    socket.emit('transcribe', transcribedText);
+    const llmResponse = await llmChat.interactWithLLM(transcribedText);
     const audioContent = await getTTSAudioContent(llmResponse.response);
     socket.emit('tts', audioContent);
-    socket.emit('ai', llmResponse?.response)
+    socket.emit('ai', llmResponse?.response);
+  });
+
+  socket.on('message', async msg => {
+    const llmResponse = await llmChat.interactWithLLM(msg);
+    const audioContent = await getTTSAudioContent(llmResponse.response);
+    socket.emit('tts', audioContent);
+    socket.emit('ai', llmResponse?.response);
   });
 });
 
---
+++ speech-to-text/index.js
@@ -0,0 +1,22 @@
+const speech = require('@google-cloud/speech');
+const { join } = require('path');
+
+const client = new speech.SpeechClient({
+  keyFile: join(__dirname, 'giggr.json'),
+});
+
+
+exports.transcribeAudio = async function (audioBuffer) {
+  const request = {
+    audio: { content: audioBuffer },
+    config: {
+      languageCode: 'en-US',
+    },
+  };
+  const [response] = await client.recognize(request);
+  console.log(response);
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  return transcription;
+};

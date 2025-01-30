69ccdde13aeff6d6366c8cc3679a8f75ad40ce7f
Junaid <junaidofficialnow@gmail.com>
2025-01-30 20:02:07 +0530

    feat: company lookup

--- enrich-profile/index.js
+++ enrich-profile/index.js
@@ -1,14 +1,56 @@
 const { default: axios } = require('axios');
 
-async function enrichUserProfile(email) {
-  const result = await axios.get(
-    'https://nubela.co/proxycurl/api/linkedin/profile/resolve/email?email=junaidofficialnow@gmail.com&first_name=junaid&last_name=Mancheri&company_domain=Askeva&enrich_profil=enrich&lookup_depth=deep&similarity_checks=skip&location=India',
-    {
-      headers: {
-        Authorization: 'Bearer'
-      }
+async function enrichUserProfile(userData) {
+  let company = '';
+  switch (userData.role) {
+    case 'Individual': {
+      const url = `https://nubela.co/proxycurl/api/linkedin/profile/resolve`;
+      const result = await axios.get(url, {
+        params: {
+          company_domain: company,
+          first_name: userData.firstName,
+          last_name: userData.lastName,
+          similarity_checks: 'skip',
+          location: 'Bangalore',
+        },
+        headers: { Authorization: 'Bearer ' + process.env.PROXY_CURL_TOKEN },
+      });
+
+      const response = await axios.get(
+        'https://nubela.co/proxycurl/api/v2/linkedin',
+        {
+          params: {
+            linkedin_profile_url: result.data.url,
+          },
+          headers: { Authorization: 'Bearer ' + process.env.PROXY_CURL_TOKEN },
+        }
+      );
+      return response.data;
     }
-  );
-  console.log(result.data, 'adf');
+
+    case 'Industry': {
+      const url = 'https://nubela.co/proxycurl/api/linkedin/company/resolve';
+      const result = await axios.get(url, {
+        params: { company_name: userData?.organization },
+        headers: { Authorization: 'Bearer ' + process.env.PROXY_CURL_TOKEN },
+      });
+      const companyLookupUrl =
+        'https://nubela.co/proxycurl/api/linkedin/company';
+      const response = await axios.get(companyLookupUrl, {
+        params: { url: result.data?.url },
+        headers: { Authorization: 'Bearer ' + process.env.PROXY_CURL_TOKEN },
+      });
+      return response.data;
+    }
+
+    case 'Institution': {
+      return {
+        no_data:
+          "the system coudn't find any data about this institution, please let the user know and continue",
+      };
+    }
+  }
 }
-enrichUserProfile();
+
+
+module.exports = enrichUserProfile;
